b05f2aba2c12aefd880ce5d94f66f1de7a2f4b9f
Jon Freynik <jonfreynik@gmail.com>
2019-10-27 14:09:08 -0400

    building functions

--- src/reader/ReaderInterface.php
+++ src/reader/ReaderInterface.php
@@ -2,9 +2,29 @@
 
 namespace hydra\text\reader;
 
+/**
+ * The job of a "Reader" is to take data from a text representation and convert 
+ * it into a data (data structured) representation.
+ *  
+ */
 interface Reader 
 {
 
-    // public function emits (); 
+    /**
+     * Returns the different typs of tokens the reader emits.
+     */
+    public function getEmits (); 
+
+    /**
+     * Loop over all of the text and emit the different tokens found.
+     */
+    public function run ();
+
+    /**
+     * Loops over all of the text and returns a serialized version of the text.
+     * If a file or stream is provided to the method, it will write the serialized 
+     * data to that stream instead of returning it, this is useful for large files. 
+     */
+    public function serialize ($file = null);
 
 }
